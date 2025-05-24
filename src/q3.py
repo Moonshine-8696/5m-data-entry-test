a545970c00593aa7c03e4dcfa99c0cfaa3adc93f
Moonshine-8696 <esee85@proton.me>
2025-05-24 08:52:46 +0800

    Update q3.py

--- src/q3.py
+++ src/q3.py
@@ -1,4 +1,11 @@
 def update_dictionary(dct, key, value):
+
+   dct = {
+  "name": "Alice",
+  "age": 25,
+   }
+    key = int(25)
+    value = int(26)
     """
     Task 1
     - Create a function that updates a dictionary (dct) with a new key-value pair.
