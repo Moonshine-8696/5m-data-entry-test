9605bac416941aab7335aed2ea9d9964f5db0270
Moonshine-8696 <esee85@proton.me>
2025-05-24 09:06:44 +0800

    Update q7.py

--- src/q7.py
+++ src/q7.py
@@ -1,4 +1,9 @@
 class Car:
+def __init__(self, make, model,year):
+    self.make = make
+    self.model = model
+    self.year = year
+    car1 = Car("Toyota","Corolla",2020)
     """
     Task 1
     - Define a class named Car with attributes: make, model, year
